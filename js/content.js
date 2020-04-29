fb618eeabba59a0f5ceea3aec9ab9cf48699cb8e
wanhose <wanhose.development@gmail.com>
2020-04-29 20:16:42 +0200

    feat(content): add support for more than 20 new providers and a thousand of sites

--- js/content.js
+++ js/content.js
@@ -18,20 +18,72 @@ if (typeof chrome.app.isInstalled !== 'undefined') {
     ]);
 
     const removableElements = Array.from([
+      // CC
+      ...document.getElementsByClassName("cc-banner"),
+      ...document.getElementsByClassName("cc_banner"),
+      ...document.getElementsByClassName("cc-bottom"),
+      ...document.getElementsByClassName("cc_container"),
+      ...document.getElementsByClassName("cc-theme-block"),
+      ...document.getElementsByClassName("cc-type-info"),
+      ...document.getElementsByClassName("cc-window"),
+      // Civic
+      document.getElementById("ccc"),
+      document.getElementById("ccc-notify"),
+      document.getElementById("ccc-overlay"),
+      // CKY
+      document.getElementById('cky-consent'),
+      ...document.getElementsByClassName("cky-consent-bar"),
+      // Consent Management Provider
+      document.getElementById("cmpbox"),
+      document.getElementById("cmpbox2"),
+      // Cookie-Script
+      document.getElementById("cookiescript_injected"),
+      // cookieBAR
+      document.getElementById("cookie-bar"),
+      document.getElementById("cookie-bar-prompt"),
+      // crowdy.ai
+      document.getElementById("crowdy-flyout"),
+      document.getElementById("crowdy-privacy-snippet"),
+      document.getElementById("crowdy-privacy-widget"),
+      document.getElementById("crowdy-privacy-widget-background"),
       // ENS
       document.getElementById("ensNotifyBanner"),
+      // Google
+      document.getElementById("cookie-statement"),
+      // iubenda
+      document.getElementById("iubenda-cs-banner"),
+      ...document.getElementsByClassName("iubenda-cs-bottom"),
+      ...document.getElementsByClassName("iubenda-cs-container"),
+      ...document.getElementsByClassName("iubenda-cs-default"),
       // Quantcast
       ...document.getElementsByClassName("qc-cmp-ui-container"),
       // OneTrust
       document.getElementById("onetrust-consent-sdk"),
       // Optanon
       ...document.getElementsByClassName("optanon-alert-box-wrapper"),
+      // Termly
+      document.getElementById("consent"),
+      // Miscellaneous
+      document.getElementById("cconsent-bar"),
+      document.getElementById("cookie-consent-banner"),
+      document.getElementById("cookie-law-info-bar"),
+      document.getElementById("cookie-law-info-again"),
+      document.getElementById("cookie-notice"),
+      ...document.getElementsByClassName("cookies"),
+      document.getElementById("cookieNotice"),
+      ...document.getElementsByClassName("fot-fixd"),
+      ...document.getElementsByClassName("js-cookies"),
     ]);
 
     notRemovableElements.forEach(element => {
       // Quantcast
       element.classList.remove("qc-cmp-ui-showing");
     });
+
+    // Fix stucked pages
+    document.body.style.overflow = '';
+    document.body.style.overflowX = '';
+    document.body.style.overflowY = '';
     
     removableElements.forEach(element => !!element && element.remove());
   };
