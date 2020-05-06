59f7f8748379e96fcb0ac460ddf48a37885a315d
wanhose <wanhose.development@gmail.com>
2020-05-06 23:13:11 +0200

    feat(content): add more providers

--- js/content.js
+++ js/content.js
@@ -13,11 +13,18 @@ if (typeof chrome.app.isInstalled !== 'undefined') {
   // Remover
   const doMagic = () => {
     const notRemovableElements = Array.from([
+      // PLO
+      ...document.getElementsByClassName("plu-no-scroll"),
       // Quantcast
       ...document.getElementsByClassName("qc-cmp-ui-showing"),
+      // Miscellaneous
+      ...document.getElementsByClassName("_2LLC6zrbk-vsnF0seit6vi"),
+      ...document.getElementsByClassName("gdpr"),
     ]);
 
     const removableElements = Array.from([
+      // BLQ
+      document.getElementById("blq-global"),
       // CC
       ...document.getElementsByClassName("cc-banner"),
       ...document.getElementsByClassName("cc_banner"),
@@ -36,6 +43,7 @@ if (typeof chrome.app.isInstalled !== 'undefined') {
       // Consent Management Provider
       document.getElementById("cmpbox"),
       document.getElementById("cmpbox2"),
+      document.getElementById("cmpContainer"),
       // Cookie-Script
       document.getElementById("cookiescript_injected"),
       // cookieBAR
@@ -46,12 +54,18 @@ if (typeof chrome.app.isInstalled !== 'undefined') {
       document.getElementById("crowdy-privacy-snippet"),
       document.getElementById("crowdy-privacy-widget"),
       document.getElementById("crowdy-privacy-widget-background"),
+      // eni.com
+      document.getElementById("eni-cookie-banner"),
       // ENS
       document.getElementById("ensNotifyBanner"),
       // ePrivacy
       document.getElementById("ePrivacyDisclaimer"),
+      // Facebook
+      ...document.getElementsByClassName("fbPageBanner"),
       // Google
       document.getElementById("cookie-statement"),
+      // HS
+      document.getElementById("hs-eu-cookie-confirmation"),
       // iubenda
       document.getElementById("iubenda-cs-banner"),
       ...document.getElementsByClassName("iubenda-cs-bottom"),
@@ -59,10 +73,15 @@ if (typeof chrome.app.isInstalled !== 'undefined') {
       ...document.getElementsByClassName("iubenda-cs-default"),
       // Moove
       document.getElementById("moove_gdpr_cookie_info_bar"),
+      // NHS
+      document.getElementById("nhsuk-cookie-banner"),
       // OneTrust
       document.getElementById("onetrust-consent-sdk"),
       // Optanon
       ...document.getElementsByClassName("optanon-alert-box-wrapper"),
+      // PLO
+      ...document.getElementsByClassName("plo-cookie-overlay"),
+      ...document.getElementsByClassName("plo-overlay"),
       // Quantcast
       ...document.getElementsByClassName("qc-cmp-ui-container"),
       // Termly
@@ -70,24 +89,50 @@ if (typeof chrome.app.isInstalled !== 'undefined') {
       // Wordpress
       document.getElementById("catapult-cookie-bar"),
       // Miscellaneous
+      ...document.getElementsByClassName("_1ouSF3xnwUjIOquxopuxSZ"),
+      ...document.getElementsByClassName("announcements"),
       ...document.getElementsByClassName("cc-cookies"),
       document.getElementById("cconsent-bar"),
       document.getElementById("cookie-consent-banner"),
+      document.getElementById("cookie-div"),
       document.getElementById("cookie-law-info-bar"),
       document.getElementById("cookie-law-info-again"),
       document.getElementById("cookie-notice"),
-      ...document.getElementsByClassName("cookies"),
+      ...document.getElementsByClassName("cookie-permission"),
+      document.getElementById("cookie-policy"),
+      document.getElementById("cookie-policy-consent"),
+      document.getElementById("cookie1"),
+      ...document.getElementsByClassName("cookiebar"),
+      document.getElementById("cookieLayer"),
       document.getElementById("cookieNotice"),
+      document.getElementById("cookieNotificationBannerWrapper"),
+      ...document.getElementsByClassName("cookies-notice"),
+      ...document.getElementsByClassName("cookies"),
+      ...document.getElementsByClassName("cookiesms"),
+      document.getElementById("cp-dialog"),
+      document.getElementById("cp-overlay"),
       ...document.getElementsByClassName("fot-fixd"),
-      ...document.getElementsByClassName("gdpr"),
+      document.getElementById("global-cookie-message"),
+      ...document.getElementsByClassName("grp-header__cookiedisclaimer"),
       ...document.getElementsByClassName("js-cookies"),
+      document.getElementById("js-gdpr-consent-banner"),
+      ...document.getElementsByClassName("pam"),
+      document.getElementById("sd-cmp"),
+      ...document.getElementsByClassName("security-policy"),
+      document.getElementById("sliding-popup"),
       document.getElementById("softMessages-list"),
       ...document.getElementsByClassName("softMessages-list"),
+      document.getElementById("u_0_1"),
     ]);
 
     notRemovableElements.forEach(element => {
+      // PLO
+      element.classList.remove("plu-no-scroll");
       // Quantcast
       element.classList.remove("qc-cmp-ui-showing");
+      // Miscellaneous
+      element.classList.remove("_2LLC6zrbk-vsnF0seit6vi");
+      element.classList.remove("gdpr");
     });
 
     // Fix stucked pages
