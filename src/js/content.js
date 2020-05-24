3a42195229e6cb8ae1a949145ea11e180b9c594e
wanhose <wanhose.development@gmail.com>
2020-05-24 19:31:01 +0200

    revert: firefox support from now

--- src/js/content.js
+++ src/js/content.js
@@ -2,13 +2,13 @@
 
 if (!!window.chrome && navigator.vendor.includes("Google")) {
   const head = document.head || document.getElementsByTagName('head')[0];
-  const runtime = browser.runtime || chrome.runtime;
   
   // Create logic script
   const script = document.createElement("script");
-  script.setAttribute("src", runtime.getURL("js/modules/script.js"));
+  script.setAttribute("src", chrome.runtime.getURL("js/modules/script.js"));
   script.setAttribute("type", "module");
 
   // Inject logic script
   head.appendChild(script);
 };
+
