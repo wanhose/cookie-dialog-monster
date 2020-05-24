0dd361e135887aeae01bd82188decacf69baa15b
wanhose <wanhose.development@gmail.com>
2020-05-24 19:11:52 +0200

    feat(content): add support for mozilla firefox

--- src/js/content.js
+++ src/js/content.js
@@ -1,12 +1,12 @@
 'use strict';
 
 if (!!window.chrome && navigator.vendor.includes("Google")) {
-  // Get document head
   const head = document.head || document.getElementsByTagName('head')[0];
+  const runtime = browser.runtime || chrome.runtime;
   
   // Create logic script
   const script = document.createElement("script");
-  script.setAttribute("src", chrome.runtime.getURL("js/modules/script.js"));
+  script.setAttribute("src", runtime.getURL("js/modules/script.js"));
   script.setAttribute("type", "module");
 
   // Inject logic script
