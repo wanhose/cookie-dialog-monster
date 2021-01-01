a6ac7c074455a5673ccdc2941ec23a75816342b1
Juan José Vílchez <wanhose.development@gmail.com>
2021-01-02 00:22:40 +0100

    feat(content): improve script

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,40 +1,40 @@
 if (!!window.chrome) {
-  const currentUrl = document.location.hostname;
+  const fix = () => {
+    document.body.style = "overflow: auto!important;";
+  };
+
+  const observe = () => {
+    observer.observe(document.body, {
+      attributes: true,
+      childList: true,
+    });
+  };
+
+  const observer = new MutationObserver((mutations, observer) => {
+    mutations.forEach(async () => {
+      observer.disconnect();
+      fix();
+      await remove();
+      observe();
+    });
+  });
+
+  const remove = async () => {
+    const filtersUrl = chrome.runtime.getURL("filters/index.txt");
+    const text = await fetch(filtersUrl).then((res) => res.text());
+    const filters = text.split("\n");
+
+    filters.forEach((match) => {
+      const element = document.querySelector(match);
+
+      if (element && element.tagName !== "BODY" && element.tagName !== "HTML") {
+        element.remove();
+      }
+    });
+  };
 
   (async () => {
-    const observer = new MutationObserver((mutations, observer) => {
-      mutations.forEach(async () => {
-        observer.disconnect();
-        await remove();
-        observe();
-      });
-    });
-
-    const observe = () => {
-      observer.observe(document.body, {
-        attributes: true,
-        childList: true,
-      });
-    };
-
-    const remove = async () => {
-      const filtersUrl = chrome.runtime.getURL("filters/index.txt");
-      const text = await fetch(filtersUrl).then((res) => res.text());
-      const filters = text.split("\n");
-
-      filters.forEach((match) => {
-        const element = document.querySelector(match);
-
-        if (
-          element &&
-          element.tagName !== "BODY" &&
-          element.tagName !== "HTML"
-        ) {
-          element.remove();
-        }
-      });
-    };
-
+    fix();
     await remove();
     observe();
   })();
